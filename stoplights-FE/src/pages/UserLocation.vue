d3ea156f9221275d98c2c8602dfa3ec2a407f2a3
github username <github email address>
2022-04-11 12:47:00 -0600

    geolocation working

--- stoplights-FE/src/pages/UserLocation.vue
+++ stoplights-FE/src/pages/UserLocation.vue
@@ -1,3 +1,49 @@
 <template>
-  <h1>User Location Page</h1>
+  <section class="ui two column centered grid">
+    <div class="column purple">
+      <form class="ui segment large form">
+        <div class="ui message red"></div>
+        <div class="ui segment">
+          <div class="field">
+            <div class="ui right icon input large">
+              <input type="text" placeholder="Enter your address"/>
+              <i class="dot circle link icon" @click="locatorButtonPressed"></i>
+            </div>
+          </div>
+          <button class="ui button">Go</button>
+        </div>
+      </form>
+    </div>
+    
+  </section>
 </template>
+<script>
+export default {
+  methods: {
+    locatorButtonPressed() {
+      if(navigator.geolocation) {
+         navigator.geolocation.getCurrentPosition(
+           position => {
+           console.log(position.coords.latitude)
+           console.log(position.coords.longitude)
+         },
+         error => {
+           console.log(error.message);
+         }
+         );
+      } else {
+        console.log("Your browser does not support geolocation API")
+      }
+    }
+  }
+}
+</script>
+
+
+<style>
+.ui.button,
+.dot.circle.icon {
+  background-color: purple;
+  color: lavenderblush ;
+}
+</style>
